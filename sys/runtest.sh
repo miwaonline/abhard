1f14f404c0fd2cda90b11f51bcc93038f155d843
Misha Masyk <mmasyk@whispir.com>
2022-10-23 20:40:20 +1100

    Updated runtest with new abilities

--- sys/runtest.sh
+++ sys/runtest.sh
@@ -7,6 +7,15 @@ curl -H "Content-Type: application/json" -X POST -d '{"cashier":"Test2", "test_m
 curl -H "Content-Type: application/json" -X POST -d '{"doc_id":"788836", "shift_id":"3", "test_mode": "1" }' localhost:8080/api/rro/eusign/receipt/1
 curl -H "Content-Type: application/json" -X POST -d '{"doc_id":"788837", "shift_id":"3", "test_mode": "1" }' localhost:8080/api/rro/eusign/receipt/1
 
+# service cashin/cashout
+curl -H "Content-Type: application/json" -X POST -d '{"doc_id":"123456", "shift_id":"3", "test_mode": "1" }' localhost:8080/api/rro/eusign/cashinout/1
+
+# check current shift totals
+curl localhost:8080/api/rro/eusign/cmd/1/LastShiftTotals | python3 -m json.tool
+
+# check particular shift full doc list
+curl localhost:8080/api/rro/eusign/cmd/1/Documents/566246891 | python3 -m json.tool
+
 # publish a report for shift id 2 from rro id 1 in a test mode
 curl -H "Content-Type: application/json" -X POST -d '{"shift_id":"3","test_mode":"1"}' localhost:8080/api/rro/eusign/zreport/1
 
