ee054a3d7386d5c6c2248a39af431894237b76c5
Misha Masyk <mmasyk@whispir.com>
2023-05-07 15:42:58 +1000

    Added some notes into runtest.sh

--- sys/runtest.sh
+++ sys/runtest.sh
@@ -14,9 +14,13 @@ curl -H "Content-Type: application/json" -X POST -d '{"cashier":"Test2", "test_m
 # we need to extract the proper value here of course
 SHIFTID=3
 
-# fiscalise a few checks
-curl -H "Content-Type: application/json" -X POST -d '{"doc_id":"788836", "shift_id":"$SHIFTID", "test_mode": "1" }' localhost:8080/api/rro/eusign/receipt/1
-curl -H "Content-Type: application/json" -X POST -d '{"doc_id":"788837", "shift_id":"$SHIFTID", "test_mode": "1" }' localhost:8080/api/rro/eusign/receipt/1
+# fiscalise a new check
+# update out_check set rro_status = 1 where id = $CHECKID
+curl -H "Content-Type: application/json" -X POST -d '{"doc_id":"$CHECKID", "shift_id":"$SHIFTID", "test_mode": "1" }' localhost:8080/api/rro/eusign/receipt/1
+
+# cancel some check
+#insert into rro_docs(RRO_ID,SHIFT_ID,CHECK_ID,DOC_TYPE,DOC_SUBTYPE,DOC_SUM) values(:rroid, :shiftid, new.id, 0, 5, new.sll_summ)
+curl -H "Content-Type: application/json" -X POST -d '{"doc_id":"$CHECKID", "shift_id":"$SHIFTID", "test_mode": "1" }' localhost:8080/api/rro/eusign/receiptcancel/1
 
 # service cashin/cashout
 curl -H "Content-Type: application/json" -X POST -d '{"doc_id":"123456", "shift_id":"$SHIFTID", "test_mode": "1" }' localhost:8080/api/rro/eusign/cashinout/1
