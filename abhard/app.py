7d86f8d6e1c0e91a1c439df4a8bb025b1c81a787
miwaonline <miwaonline@gmail.com>
2024-10-18 18:15:12 +1100

    Added conditional loading for scaner and PRRO modules

--- abhard/app.py
+++ abhard/app.py
@@ -3,32 +3,34 @@ import signal
 import sys
 from waitress import serve
 from sysutils import main_logger, config
-from rro_eusign import EUSign
-from scaner_thread import ScanerThread, TCPSocketThread
 from api import api, rro_objects, tcpsocket_threads, scaner_threads
 
 app = Flask("abhard")
 app.register_blueprint(api)
 
 # Initialize RRO objects
-for rro in config["rro"]:
-    rroobj = EUSign(rro["id"], rro["keyfile"], rro["keypass"])
-    rro_objects[rro["id"]] = rroobj
-    main_logger.info(f"Initialized RRO {rro['id']}")
+if config.get("rro"):
+    from rro_eusign import EUSign
+    for rro in config["rro"]:
+        rroobj = EUSign(rro["id"], rro["keyfile"], rro["keypass"])
+        rro_objects[rro["id"]] = rroobj
+        main_logger.info(f"Initialized RRO {rro['id']}")
 
 # Initialize Scaner threads
-for scaner in config["scaner"]:
-    if scaner["type"] == "serial":
-        main_logger.info(f"Starting TCP socket thread for {scaner['name']}")
-        tcpthread = TCPSocketThread(
-            scaner["name"], scaner["socket_port"]
-        )
-        tcpsocket_threads[scaner["name"]] = tcpthread
-        tcpthread.start()
-        main_logger.info(f"Starting listening thread for {scaner['name']}")
-        thread = ScanerThread(scaner["name"], scaner["device"], tcpthread)
-        scaner_threads[scaner["name"]] = thread
-        thread.start()
+if config.get("scaner"):
+    from scaner_thread import ScanerThread, TCPSocketThread
+    for scaner in config["scaner"]:
+        if scaner["type"] == "serial":
+            main_logger.info(f"Starting TCP thread for {scaner['name']}")
+            tcpthread = TCPSocketThread(
+                scaner["name"], scaner["socket_port"]
+            )
+            tcpsocket_threads[scaner["name"]] = tcpthread
+            tcpthread.start()
+            main_logger.info(f"Starting listening thread for {scaner['name']}")
+            thread = ScanerThread(scaner["name"], scaner["device"], tcpthread)
+            scaner_threads[scaner["name"]] = thread
+            thread.start()
 
 
 def signal_handler(sig, frame):
@@ -44,5 +46,4 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=config["webservice"]["port"])
     serve(app, host="0.0.0.0", port=config["webservice"]["port"])
