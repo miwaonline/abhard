62962a04ad295c85d0a322d531bfe721d9d262f1
miwaonline <miwaonline@gmail.com>
2024-10-19 20:06:32 +1100

    Another win.py rework

--- abhard/win.py
+++ abhard/win.py
@@ -3,11 +3,9 @@ import win32service
 import win32event
 import servicemanager
 import socket
-import subprocess
 from app import app
 from waitress import serve
 from sysutils import config
-import sys
 
 
 class AbhardService(win32serviceutil.ServiceFramework):
@@ -22,11 +20,13 @@ class AbhardService(win32serviceutil.ServiceFramework):
         win32serviceutil.ServiceFramework.__init__(self, args)
         self.stop_event = win32event.CreateEvent(None, 0, 0, None)
         socket.setdefaulttimeout(60)
+        self.isrunning = False
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         win32event.SetEvent(self.stop_event)
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
+        self.isrunning = False
 
     def SvcDoRun(self):
         servicemanager.LogMsg(
@@ -34,29 +34,12 @@ class AbhardService(win32serviceutil.ServiceFramework):
             servicemanager.PYS_SERVICE_STARTED,
             (self._svc_name_, ""),
         )
+        self.isrunning = True
         self.run_flask_app()
 
     def run_flask_app(self):
         serve(app, host="0.0.0.0", port=config["webservice"]["port"])
 
 
-def install_service():
-    service_name = "Abhard"
-    win32serviceutil.InstallService(
-        AbhardService._svc_name_,
-        AbhardService._svc_display_name_,
-        AbhardService._svc_description_,
-    )
-    win32serviceutil.StartService(service_name)
-    subprocess.run(f"sc config {service_name} start= auto", shell=True, check=True)
-    subprocess.run(
-        f"sc failure {service_name} reset= 60 actions= restart/60000",
-        shell=True,
-    )
-
-
 if __name__ == "__main__":
-    if "install" in sys.argv:
-        install_service()
-    else:
-        win32serviceutil.HandleCommandLine(AbhardService)
+    win32serviceutil.HandleCommandLine(AbhardService)
