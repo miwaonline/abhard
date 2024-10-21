6fb4fbd27f1e6b3869983fbbc168935e2fcf36b8
miwaonline <miwaonline@gmail.com>
2024-10-21 18:50:14 +1100

    Enhancing error message logging

--- abhard/app.py
+++ abhard/app.py
@@ -98,7 +98,8 @@ if os.name == "nt":
             try:
                 main()
             except Exception as e:
-                servicemanager.LogErrorMsg(e.message)
+                servicemanager.LogErrorMsg(e)
+                servicemanager.LogErrorMsg(e.args)
                 servicemanager.LogErrorMsg(traceback.format_exc())
                 os._exit(-1)
 
